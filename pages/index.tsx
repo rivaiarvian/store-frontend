cf414139f163018bb9cb9d6ed27c3ed7c2e05298
rivaiarvian <rivai.arviannn@gmail.com>
2022-03-30 13:46:24 +0700

    basic SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import Head from "next/head";
 import AOS from "aos";
 import Navbar from "../components/organisms/Navbar";
 import MainBanner from "../components/organisms/MainBanner";
@@ -14,6 +15,23 @@ export default function Home() {
   }, []);
   return (
     <>
+      <Head>
+        <title>Store - Get a New Experience in Gaming</title>
+        <meta
+          name="description"
+          content="Kami menyediakan jutaan cara untuk membantu players menjadi pemenang sejati"
+        />
+        <meta
+          property="og:title"
+          content="Store - Get a New Experience in Gaming"
+        />
+        <meta
+          property="og:description"
+          content="Kami menyediakan jutaan cara untuk membantu players menjadi pemenang sejati"
+        />
+        <meta property="og:image" content="" />
+        <meta property="og:url" content="https://storeShanum.com" />
+      </Head>
       <Navbar />
       <MainBanner />
       <TransactionStep />
